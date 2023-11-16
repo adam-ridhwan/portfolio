1ed98cd7f69af2c06563fcf97c0921c7f55f689d
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-11-16 10:57:43 -0500

    add ring to skills

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,31 +1,39 @@
+import { ReactNode } from 'react';
+
 import { H2, P } from '@/components/typography';
+
+const Highlight = ({ children }: { children: ReactNode }) => (
+  <span className='text-secondary-foreground'>{children}</span>
+);
 
 export const About = () => {
   return (
-    <>
-      <section className='relative'>
-        <H2>ABOUT</H2>
+    <section className='relative'>
+      <H2>ABOUT</H2>
 
-        <P>
-          Hi, I&apos;m Adam, a Software Engineer based in Boston, Massachusetts. I build responsive and modern
-          web applications that is user-centric ranging from blog applications to ecommerce platforms. I am
-          passionate about coding and being pixel perfect using beautiful interfaces with intuitively
-          implemented UX design.
-        </P>
+      <P>
+        Hi, I&apos;m <Highlight>Adam</Highlight> 👋 I&apos;m{' '}
+        <Highlight>Fullstack Software Engineer</Highlight> based in{' '}
+        <Highlight>Boston, Massachusetts</Highlight>.
+      </P>
 
-        <P>
-          All my life I have been immersed in the world of music. I grew up playing the piano and the electric
-          guitar, and loved performing on stage. So I decided to pursue my music degree. One of the classes I
-          enjoyed was not music related at all. I took a Web Development class, which piqued my interest on
-          building websites. Ever since then, I went on a deep dive on coding and web development. I then
-          pivoted into the tech industry and have been coding ever since.
-        </P>
+      <P>
+        I build responsive and modern web applications that is user-centric ranging from blog applications to
+        ecommerce platforms. I am passionate about coding and being pixel perfect using beautiful UI with
+        intuitively implemented UX design.
+      </P>
 
-        <P className='m-0'>
-          When I am not coding on my computer, I like to play badminton on the weekends, chilling at home with
-          my cats, or go hiking in the 4000ft peaks at New Hampshire.
-        </P>
-      </section>
-    </>
+      <P>
+        My passion for coding started during my music degree. One of the classes I enjoyed was not music
+        related at all. I took a Web Development class, which piqued my interest on building websites. Ever
+        since then, I went on a deep dive on coding and web development. Now, I strive to learn as much as I
+        can and make great code.
+      </P>
+
+      <P className='m-0'>
+        When I am not coding on my computer, I like to play badminton on the weekends with my girlfriend,
+        chilling at home with my cats, or go hiking in the 4000ft peaks at New Hampshire.
+      </P>
+    </section>
   );
 };
