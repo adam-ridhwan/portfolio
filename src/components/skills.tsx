26ea780d24c974db6bd5ce6cc45937a71201ebb7
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-11-16 10:35:28 -0500

    add ring to skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -11,18 +11,14 @@ export const Skills = () => {
 
       <P>
         Not to give a generic answer, but I truly think the most suitable technology for a task is the one
-        that fits best.
-      </P>
-
-      <P>
-        However, there are certain technologies I particularly enjoy using. My choice tech stack for personal
-        projects is primarily:
+        that fits best. However, there are certain technologies I particularly enjoy using. My choice tech
+        stack for personal projects is primarily:
       </P>
 
       <div className='mt-2 flex flex-wrap justify-center gap-2 text-center'>
         {TECHNOLOGIES.map(({ Icon, label }) => (
-          <Badge key={label} variant='secondary' className='text-md gap-1 font-mono font-normal'>
-            <Icon className='h-4 w-4' />
+          <Badge key={label} variant='secondary' className='text-md gap-1 font-mono font-normal ring-1'>
+            <Icon className='h-5 w-5' />
             <span>{label}</span>
           </Badge>
         ))}
