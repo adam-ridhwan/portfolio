2ce34661bcc9bf4a3bf2349fea73d1a36c199e03
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-11-18 22:21:35 -0500

    refactor

--- src/app/api/clicks/route.ts
+++ src/app/api/clicks/route.ts
@@ -7,9 +7,9 @@ export async function POST(request: NextRequest) {
     ip: request.headers.get('x-real-ip') ?? '127.0.0.1',
     message: 'Hello from the API',
   };
-  const ipAddress =
-    request.headers['x-forwarded-for'] ||
-    // Fallback for localhost or non Vercel deployments
-    '0.0.0.0';
-  return new NextResponse(JSON.stringify(ipAddress, null, 2));
+  // const ipAddress =
+  //   request.headers['x-forwarded-for'] ||
+  //   // Fallback for localhost or non Vercel deployments
+  //   '0.0.0.0';
+  return new NextResponse(JSON.stringify(data, null, 2));
 }
