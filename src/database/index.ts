06acf12ae823f82ba7d26e0c01619916441c545e
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-11-21 14:55:35 -0500

    refactor

--- src/database/index.ts
+++ src/database/index.ts
@@ -60,7 +60,7 @@ export const PROJECTS: Project[] = [
     description: `An image generator platform with an input that allows users to generate any image based on the prompt.`,
     type: 'AI image generator',
     image: '/pixel-craft.png',
-    link: 'https://pixel-craft-rust.vercel.app/',
+    link: 'https://github.com/adam-ridhwan/pixel-craft',
   },
   {
     id: '1',
@@ -68,7 +68,7 @@ export const PROJECTS: Project[] = [
     description: `A blogging platform where users can post articles. Features include the ability for readers to like, comment on, save, and delete posts.`,
     type: 'Blog',
     image: '/pondero.png',
-    link: 'https://pondero-sigma.vercel.app',
+    link: 'https://github.com/adam-ridhwan/pondero',
   },
   {
     id: '2',
@@ -76,7 +76,7 @@ export const PROJECTS: Project[] = [
     description: `An e-commerce website with a dynamic shopping cart, product filtering, a custom image gallery, and a seamless Stripe checkout process.`,
     type: 'Ecommerce',
     image: '/flair.png',
-    link: 'https://flair.vercel.app',
+    link: 'https://github.com/adam-ridhwan/flair',
   },
   {
     id: '3',
@@ -84,7 +84,7 @@ export const PROJECTS: Project[] = [
     description: `An interactive quiz platform designed to make quizzes easy. It offers a seamless experience for educators, students, and trivia enthusiasts alike.`,
     type: 'Quiz',
     image: '/quizzzy.png',
-    link: 'https://quizzzy-mu.vercel.app/',
+    link: 'https://github.com/adam-ridhwan/quizzzy/',
   },
   {
     id: '4',
@@ -92,6 +92,6 @@ export const PROJECTS: Project[] = [
     description: `A TV streaming platform offering a library of live tv shows. Features include a custom video player, url routing, and a responsive design.`,
     type: 'TV streaming',
     image: '/geaux.png',
-    link: 'https://geaux-tv.vercel.app/tv/465',
+    link: 'https://github.com/adam-ridhwan/geaux-tv',
   },
 ];
